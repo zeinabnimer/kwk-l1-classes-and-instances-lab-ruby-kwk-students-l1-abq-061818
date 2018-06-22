97f023dc1fcaa73796c8c6dca3a900ad3bedaab2
zeinabnimer <www.zeinab6@gmail.com>
2018-06-22 16:46:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,13 @@
 # person.rb
+class person
+  def initialize(name)
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+end
+adele_goldberg = Dog.new(adele_goldberg)
+alan_key = Dog.new(alan_kay)
